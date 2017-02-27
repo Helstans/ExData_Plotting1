6fc54947973c46d63dbfd9b5efe54fcdd167fa98
Helstans <helen.c.stansfield@gmail.com>
2017-02-27 21:31:55 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,3 +1,15 @@
+## Load data and extract dates
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+data <- subset(data,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+data <- data[complete.cases(data),]
+dateTime <- paste(data$Date, data$Time)
+dateTime <- setNames(dateTime, "DateTime")
+data <- data[ ,!(names(data) %in% c("Date","Time"))]
+data <- cbind(dateTime, data)
+data$dateTime <- as.POSIXct(dateTime)
+
+## Plot graphs
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,0,0))
 with(data, {
   plot(Global_active_power~dateTime, type="l", 
